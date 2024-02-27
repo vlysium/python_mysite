9b1fb5bf4167d96bfd303923ce5ea11b13aaded3
Victor Ly <ly.victor2000@gmail.com>
2024-02-27 13:20:37 +0100

    update git repo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import git
  
 @post('/secret_url_for_git_hook')
 def git_update():
-  repo = git.Repo('./mysite')
+  repo = git.Repo('./python_mysite')
   origin = repo.remotes.origin
   repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
   origin.pull()
